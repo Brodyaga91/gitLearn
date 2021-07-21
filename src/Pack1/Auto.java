53d72290ced211e2694c5fac52f4d236a66ed901
Brodyaga91 <brodyaga-s2008@yandex.ru>
2021-07-21 18:46:11 +0300

    task14

---
+++ src/Pack1/Auto.java
@@ -0,0 +1,14 @@
+package Pack1;
+
+public class Auto {
+	public String name;
+	public Auto(String name) {
+		this.name=name;
+	}
+	
+	public void showName() {
+		System.out.println(name);
+	}
+	
+
+}
--- src/Pack1/TestGit.java
+++
@@ -1,8 +0,0 @@
-package Pack1;
-
-public class TestGit {
-	public static void main(String[] args) {
-		System.out.println("Вывод сообщения");
-	}
-
-}
