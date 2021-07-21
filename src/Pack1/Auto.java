2158ded606016108f03eb2556a4ee24710a1952b
Brodyaga91 <brodyaga-s2008@yandex.ru>
2021-07-21 19:03:50 +0300

    task15

--- src/Pack1/Auto.java
+++ src/Pack1/Auto.java
@@ -1,14 +1,31 @@
 package Pack1;
 
 public class Auto {
-	public String name;
-	public Auto(String name) {
-		this.name=name;
+	private String model = "bmw";
+	private int power = 250;
+	private String color = "black";
+	
+	public String getModel() {
+		return model;
 	}
 	
-	public void showName() {
-		System.out.println(name);
+	public String getColor() {
+		return color;
 	}
-	
 
+	public int getPower() {
+		return power;
+	}
+	
+	public void setModel(String model) {
+		this.model=model;
+	}
+	
+	public void setColor(String color) {
+		this.color=color;
+	}
+	
+	public void setPower(int power) {
+		this.power=power;
+	}
 }
---
+++ src/Pack1/TestAuto.java
@@ -0,0 +1,9 @@
+package Pack1;
+
+public class TestAuto {
+	public static void main(String[] args) {
+		Auto test=new Auto();
+		System.out.println(test.getPower());
+	}
+
+}
