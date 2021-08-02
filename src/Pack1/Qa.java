1d665e2b030cfa431d5c4b63d445c940d1499593
Brodyaga91 <brodyaga-s2008@yandex.ru>
2021-08-02 19:00:45 +0300

    task17final

--- src/Pack1/Qa.java
+++ src/Pack1/Qa.java
@@ -9,24 +9,19 @@ public class Qa extends People {
 	
 		 
 	public void testing(String first, String second, String third) {
-		if(third==null) {
-			if(second==null) {
-				if(first==null) {
-					System.out.println("ok, go coffee");
-				}
-				else {
-				System.out.println("the task was received: "+first);
-				}
-			}
-			else {
-				System.out.println("two task was received: "+first+" and "+second);
-			}
-		}
-		else {
 			System.out.println("the task was received: "+first+" and "+second+" and "+third);
-		}
-	
+		
 	}
+	
+	public void testing(String first, String second) {
+		System.out.println("the task was received: "+first+" and "+second);
+	
+}
+	
+	public void testing(String first) {
+		System.out.println("the task was received: "+first);
+	
+}
 	
 	public void flooding() {
 		System.out.println("тостер ушел искать смешные картинки");
