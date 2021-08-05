dc79927fe788aee1bc935720b091d31a37b8f63f
Brodyaga91 <brodyaga-s2008@yandex.ru>
2021-08-06 01:39:54 +0300

    task20

---
+++ src/Pack1/Maps.java
@@ -0,0 +1,17 @@
+package Pack1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps {
+	public static void main(String[] args) {
+		HashMap<Integer, String> autor=new HashMap<>();
+		autor.put(1, "Tolstoy");
+		autor.put(2, "Chehov");
+		autor.put(3, "Pushkin");
+		System.out.println(autor.get(2));
+		System.out.println(autor.toString());
+	}
+
+}
+
